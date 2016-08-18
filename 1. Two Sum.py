3de9d506de2044a46b19825de377d08db7615163
Subodh Yadav <subodh29031995@gmail.com>
2016-08-18 08:46:44 +0530

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,3 +1,5 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution.
 class Solution(object):
     def twoSum(self, nums, target):
 	"""
