700062129720fc8de73188d70d01b0a02bbfdd09
Subodh Yadav <subodh29031995@gmail.com>
2016-08-19 14:47:13 +0530

    Add files via upload

---
+++ 344. Reverse String.py
@@ -0,0 +1,8 @@
+#Write a function that takes a string as input and returns the string reversed.
+class Solution(object):
+    def reverseString(self, s):
+	"""
+	:type s: str
+	:rtype: str
+	"""
+	return s[::-1]
