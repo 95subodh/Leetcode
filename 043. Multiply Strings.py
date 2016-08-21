a5731c14a1574db3889a2a89d973318c20bee18e
Subodh Yadav <subodh29031995@gmail.com>
2016-08-21 23:08:20 +0530

    Add files via upload

---
+++ 043. Multiply Strings.py
@@ -0,0 +1,27 @@
+#Given two numbers represented as strings, return multiplication of the numbers as a string.
+#
+#Note:
+#The numbers can be arbitrarily large and are non-negative.
+#Converting the input string to integer is NOT allowed.
+#You should NOT use internal library such as BigInteger.
+class Solution(object):
+	def multiply(self, num1, num2):
+		"""
+		:type num1: str
+		:type num2: str
+		:rtype: str
+		"""
+		num1, num2 = num1[::-1], num2[::-1]
+		res = [0] * (len(num1) + len(num2))
+		for i in xrange(len(num1)):
+			for j in xrange(len(num2)):
+				res[i + j] += int(num1[i]) * int(num2[j])
+				res[i + j + 1] += res[i + j] / 10
+				res[i + j] %= 10
+
+		# Skip leading 0s.
+		i = len(res) - 1
+		while i > 0 and res[i] == 0:
+			i -= 1
+
+		return ''.join(map(str, res[i::-1]))
