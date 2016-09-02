cebc9d32f80d5a21631ed6d1e37e775cf205e27e
Subodh Yadav <subodh29031995@gmail.com>
2016-09-02 19:05:08 +0530

    Add files via upload

---
+++ 371. Sum of Two Integers.py
@@ -0,0 +1,14 @@
+#Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+class Solution(object):
+    def getSum(self, a, b):
+		"""
+		:type a: int
+		:type b: int
+		:rtype: int
+		"""
+		MAX_INT = 0x7FFFFFFF
+		MIN_INT = 0x80000000
+		MASK = 0x100000000
+		while b:
+			a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+		return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
