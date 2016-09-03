8e23b48e1993d2c890a4e642c5a17bf2a7b662d6
Subodh Yadav <subodh29031995@gmail.com>
2016-09-03 22:50:12 +0530

    Add files via upload

---
+++ 225. Implement Stack using Queues.py
@@ -0,0 +1,43 @@
+#Implement the following operations of a stack using queues.
+#
+#push(x) -- Push element x onto stack.
+#pop() -- Removes the element on top of the stack.
+#top() -- Get the top element.
+#empty() -- Return whether the stack is empty.
+#Notes:
+#You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+#Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+#You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+
+class Stack(object):
+	def __init__(self):
+		"""
+		initialize your data structure here.
+		"""
+		self.stack=[]
+
+	def push(self, x):
+		"""
+		:type x: int
+		:rtype: nothing
+		"""
+		self.stack.append(x)
+		
+
+	def pop(self):
+		"""
+		:rtype: nothing
+		"""
+		self.stack.pop()
+
+	def top(self):
+		"""
+		:rtype: int
+		"""
+		return self.stack[-1]
+
+	def empty(self):
+		"""
+		:rtype: bool
+		"""
+		return False if len(self.stack) else True
