28f910e85f2d5763584b461d5b2c084d16f5644c
Subodh Yadav <subodh29031995@gmail.com>
2016-09-26 19:30:43 +0530

    Add files via upload

---
+++ 022. Generate Parentheses.py
@@ -0,0 +1,27 @@
+#Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+#
+#For example, given n = 3, a solution set is:
+#
+#[
+#	"((()))",
+#	"(()())",
+#	"(())()",
+#	"()(())",
+#	"()()()"
+#]
+
+class Solution(object):
+    def generateParenthesis(self, n):
+		"""
+		:type n: int
+		:rtype: List[str]
+		"""
+		l=[]
+		def gen(s,r,n):
+			if n==0:
+				l.append(s)
+			else: 
+				if n>r:gen(s+"(",r+1,n-1)
+				if r>0:gen(s+")",r-1,n-1)
+		gen("",0,n*2)
+		return l
