f59fb66b92a7979b484a1090942233a6dff30ee7
Subodh Yadav <subodh29031995@gmail.com>
2016-10-16 12:05:56 +0530

    Update 404. Sum of Left Leaves.py

--- 404. Sum of Left Leaves.py
+++ 404. Sum of Left Leaves.py
@@ -3,10 +3,10 @@
 #Example:
 #
 #	3
-#   / \
-#  9  20
+#      / \
+#     9  20
 #	/  \
-#   15   7
+#      15   7
 #
 #There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
 
