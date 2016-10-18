3d11ddbb2ba7326dc50c65372470ab3c2173b6c1
Subodh Yadav <subodh29031995@gmail.com>
2016-10-18 20:20:15 +0530

    Update and rename 82. Remove Duplicates from Sorted List II.py to 082. Remove Duplicates from Sorted List II.py

--- 082. Remove Duplicates from Sorted List II.py
+++ 082. Remove Duplicates from Sorted List II.py
@@ -1,8 +1,8 @@
-Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+# Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
 
-For example,
-Given 1->2->3->3->4->4->5, return 1->2->5.
-Given 1->1->1->2->3, return 2->3.
+# For example,
+# Given 1->2->3->3->4->4->5, return 1->2->5.
+# Given 1->1->1->2->3, return 2->3.
 
 # Definition for singly-linked list.
 # class ListNode(object):
